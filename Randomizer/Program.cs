b9f2fb75950f33496b7825d3112517c919396b18
Nikita Mikheev <54310329+slivachuu@users.noreply.github.com>
2022-09-30 01:05:30 +0300

    Add files via upload

---
+++ Randomizer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================\nRandomizer v2.0\nby slivachuu\n================");
+            int repeat_mode;
+            Console.WriteLine("Выберите режим: \n1. Одинарный режим\n2. Цикличный режим");
+            Console.Write("> ");
+            repeat_mode = Convert.ToInt32(Console.ReadLine());
+            if (repeat_mode == 1)
+            {
+                int min, max, output;
+                Console.Write("Введите минимальное число: ");
+                min = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите максимальное число: ");
+                max = Convert.ToInt32(Console.ReadLine());
+                var random = new Random();
+                output = random.Next(minValue: min, maxValue: max);
+                Console.WriteLine("Рандомное число: " + output);
+            } 
+            else if (repeat_mode == 2)
+            {
+                int repeat, min, max, output;
+                Console.Write("Введите кол-во циклов: ");
+                repeat = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите минимальное число: ");
+                min = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите максимальное число: ");
+                max = Convert.ToInt32(Console.ReadLine());
+                var random = new Random();
+                for (int i = 1; i <= repeat; i++)
+                {
+                    output = random.Next(minValue: min, maxValue: max);
+                    Console.WriteLine(i + ". Рандомное число: " + output);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
